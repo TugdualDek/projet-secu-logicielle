d72d704f1a58ea3206c727a82d2d2a59bff5133a
Redhoxx <64523199+Redhoxx@users.noreply.github.com>
2024-11-27 16:14:55 +0100

    adding BaseModule report_violation_least_priv.py

--- vulnerabilities/modules/report_violation_least_priv.py
+++ vulnerabilities/modules/report_violation_least_priv.py
@@ -1,3 +1,5 @@
+from base_module import BaseModule
+
 def report_violation(violation_type, username, resource_url):
   print(f"Violation du principe du moindre privilège détectée !")
   print(f"  Type de violation: {violation_type}")
