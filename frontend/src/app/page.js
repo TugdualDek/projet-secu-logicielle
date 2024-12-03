f0280fa9337d3e436eeabcb0ef07e830be644e7a
Diane <diane.dinh@eleve.isep.fr>
2024-12-03 12:46:50 +0100

    Radial gradient for background + fixed babel issues

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,22 +3,23 @@ import { Activity, Shield } from 'lucide-react';
 import GradientBackground from '@/components/gradient-background';
 
 export default function Home() {
-  return (
-    <div className='w-full h-screen relative overflow-hidden'>
-      <div className="h-full flex flex-1 flex-col items-center justify-center gap-5">
-        <div className='h-16 w-16 flex items-center justify-center bg-white/15 backdrop-blur-md rounded-full text-[#6E3CEC]'>
-          <Shield size={36}  />
-        </div>
-        <h1 className="text-5xl font-bold text-white">Discover how secure your website is</h1>
-        <p className="text-2xl font-medium text-neutral-500">Test the vulnerabilities in matter of seconds,<br/>avoid potential risks threats on your company.</p>
-        <div className="flex flex-row items-center bg-white/15 p-2 rounded-full mt-8">
-          <input type="text" placeholder="Enter your URL" className="w-64 bg-transparent outline-none px-2" />
-          <button className="h-10 w-10 flex items-center justify-center rounded-full text-black bg-white">
-            <Activity size={18} />
-          </button>
-        </div>
-      </div>
-      <GradientBackground />
-    </div>
-  );
+	return (
+		<div className='w-full h-screen relative overflow-hidden'>
+			<div className="absolute z-20 w-full h-full flex flex-1 flex-col items-center justify-center gap-5">
+				<div className='h-16 w-16 flex items-center justify-center bg-white/15 backdrop-blur-md rounded-full text-[#6E3CEC]'>
+					<Shield size={36}  />
+				</div>
+				<h1 className="text-5xl font-bold text-white">Discover how secure your website is</h1>
+				<p className="text-2xl font-medium text-neutral-500">Test the vulnerabilities in matter of seconds,<br/>avoid potential risks threats on your company.</p>
+				
+				<div className="flex flex-row items-center bg-white/10 p-2 rounded-full mt-8">
+					<input type="text" placeholder="Enter your URL" className="w-64 bg-transparent outline-none px-2" />
+					<button className="h-10 w-10 flex items-center justify-center rounded-full text-black bg-white">
+						<Activity size={18} />
+					</button>
+				</div>
+			</div>
+			<GradientBackground />
+		</div>
+	);
 }
--- frontend/src/components/gradient-background.jsx
+++ frontend/src/components/gradient-background.jsx
@@ -1,7 +1,7 @@
 const GradientBackground = () => {
     return (
-        <div className="absolute -z-10 inset-0 h-[200vh] backdrop-blur-[100px] bg-[#ffffff03]">
-            <div className="bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%" />
+        <div className="absolute z-10 inset-0 h-[100vh] backdrop-blur-[100px] bg-[#ffffff03]">
+            <div className="h-full bg-[radial-gradient(ellipse_100%_100%_at_bottom,rgba(0,0,0,0)_50%,rgba(106,20,255,0.5)_75%,#6A14FF_90%,#BA93FF_100%)]" />
         </div>
     );
 }
