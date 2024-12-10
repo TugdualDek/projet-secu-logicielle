f0fc6b2505fd0e5457e1bbc3de54cbc6abc415f5
dinhdiane <dianeanhduong@gmail.com>
2024-12-10 02:19:40 +0100

    Changed grid layout for a span 3 on open and span 1 on closed accordions

--- frontend/src/app/report/page.jsx
+++ frontend/src/app/report/page.jsx
@@ -29,14 +29,28 @@ const accordionData = [
 	{
 		id: 4,
 		badge: "No vulnerability",
-		title: "Injection",
+		title: "Name 4",
 		subtitle: "Security subtitle quickly describing",
 		description: "No issues detected for Injection vulnerabilities.",
 	},
 	{
 		id: 5,
 		badge: "No vulnerability",
-		title: "Injection",
+		title: "Name 5",
+		subtitle: "Security subtitle quickly describing",
+		description: "No issues detected for Injection vulnerabilities.",
+	},
+	{
+		id: 6,
+		badge: "No vulnerability",
+		title: "Name 6",
+		subtitle: "Security subtitle quickly describing",
+		description: "No issues detected for Injection vulnerabilities.",
+	},
+	{
+		id: 7,
+		badge: "No vulnerability",
+		title: "Name 7",
 		subtitle: "Security subtitle quickly describing",
 		description: "No issues detected for Injection vulnerabilities.",
 	}
@@ -50,8 +64,8 @@ const Report = () => {
 	};
 
 	return (
-		<div className='w-full max-w-[1200px] mx-auto h-auto min-h-screen flex flex-col items-center pt-[25vh] px-4 pb-4 gap-2'>
-			{/* Alert Header */}
+		<div className='w-full max-w-[1200px] mx-auto min-h-screen flex flex-col items-center pt-[25vh] px-4 pb-4 gap-2'>
+			
 			<div className="h-16 w-16 flex items-center justify-center bg-red/15 backdrop-blur-md rounded-full border border-white/15">
 				<ShieldAlert size={36} className='text-red' />
 				<ShieldCheck size={36} className='text-green hidden'/>
@@ -68,46 +82,53 @@ const Report = () => {
 			</p>
 
 			{/* Accordion Grid */}
-			<div className="grid grid-cols-2 gap-2 mt-16 w-full px-6">
-				{accordionData.map((item) => (
-				<div
-					key={item.id}
-					className={`flex flex-col justify-between group border border-white/15 rounded-lg p-8 gap-8 relative transition-all duration-300 hover:bg-white/5 cursor-pointer
-						${openAccordion === item.id ? "col-span-2" : "col-span-1"} 
-					}`}
-					onClick={() => toggleAccordion(item.id)}
-				>
-					<div className="flex flex-row w-full items-center justify-between">
-						{/* Badge */}
-						<span className="text-xs font-semibold uppercase text-red-400">
-							{item.badge}
-						</span>
+			<div className="grid grid-cols-3 gap-2 mt-16 w-full px-6">
+				{accordionData.map((item) => {
+					const isOpen = openAccordion === item.id;
 
-						{/* Chevron */}
-						<div className="opacity-0 transition duration-300 group-hover:opacity-100">
-							<ChevronDown size={20} className="text-white" />
+					return (
+						<div
+							key={item.id}
+							className={`flex flex-col justify-between group border border-white/15 rounded-lg p-8 gap-8 relative transition-all duration-300 hover:bg-white/5 cursor-pointer 
+								${isOpen ? "col-span-3 bg-white/5" : "col-span-1"}
+							`}
+							onClick={() => toggleAccordion(item.id)}
+						>
+							<div className="flex w-full items-center justify-between">
+								
+								{/* Badge */}
+								<span className="text-xs font-semibold uppercase text-red-400">
+									{item.badge}
+								</span>
+
+								{/* Chevron */}
+								<div className="opacity-0 transition duration-300 group-hover:opacity-100">
+									<ChevronDown size={20} className="text-white" />
+								</div>
+
+							</div>
+
+							{/* Title and Subtitle */}
+							<div className="w-full flex flex-col items-start gap-2">
+								<h2 className="text-white text-2xl font-semibold mt-2">
+									{item.title}
+								</h2>
+
+								<p className="text-foreground-secondary text-sm">
+									{item.subtitle}
+								</p>
+
+								{/* Paragraph */}
+								{isOpen && (
+									<p className="text-white mt-4">
+										{item.description}
+									</p>
+								)}
+								
+							</div>
 						</div>
-					</div>
-
-					{/* Title and Subtitle */}
-					<div className="w-full flex flex-col items-start gap-2">
-						<h2 className="text-white text-2xl font-semibold mt-2">
-							{item.title}
-						</h2>
-						<p className="text-foreground-secondary text-sm">
-							{item.subtitle}
-						</p>
-
-						{/* Paragraph */}
-						{openAccordion === item.id && (
-						<p className="text-white mt-4">
-							{item.description}
-						</p>
-						)}
-					</div>
-
-				</div>
-				))}
+					);
+				})}
 			</div>
 		</div>
 	);
