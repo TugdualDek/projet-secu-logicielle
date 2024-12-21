c69f26f4317b1565928e009e3c25c7772ad8282d
dinhdiane <dianeanhduong@gmail.com>
2024-12-21 17:17:59 +0100

    Input form url submit

--- frontend/src/components/search-input.jsx
+++ frontend/src/components/search-input.jsx
@@ -1,20 +1,48 @@
+"use client";
+
 import { Activity } from 'lucide-react';
+import { useState } from "react";
+import { scanService } from '@/services/api';
 
 const SearchInput = ({ onClick }) => {
+
+    const [url, setUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            await scanService.createScan(url);
+            window.dispatchEvent(new Event('scanCreated'));
+        } catch (error) {
+            console.error('Erreur lors de la création du scan:', error);
+        } finally {
+            setLoading(false);
+            setUrl("");
+        }
+    };
+
     return (
-        <div className="flex flex-row items-center bg-white/10 p-2 rounded-full border border-white/15 mt-8">
+        <form onSubmit={handleSubmit} className="flex flex-row items-center bg-white/10 p-2 rounded-full border border-white/15 mt-8">
             <input
-                type="text"
+                id='url'
+                type="url"
                 placeholder="Enter your URL"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                required
                 className="w-72 bg-transparent outline-none px-2"
             />
             <button
+                type="submit"
                 onClick={onClick}
                 className="h-10 w-10 flex items-center justify-center rounded-full text-black bg-white"
             >
-                <Activity size={18} />
+                {loading ? 'Loading...' : <Activity size={18} /> }
+                
             </button>
-        </div>
+        </form>
     );
 };
 
